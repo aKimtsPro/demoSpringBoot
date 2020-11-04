973011a90397ceca75ba1940d8b1ef48e1dde21a
akimt <a.kimtsaris@outlook.com>
2020-11-04 16:20:51 +0100

    commit - 2 :
    - demo creation de beans
    - exo creation de beans(phase d'énoncé)

--- src/main/java/bstrom/akim/demoSpringBoot/exo/InterfaceConsoleExo.java
+++ src/main/java/bstrom/akim/demoSpringBoot/exo/InterfaceConsoleExo.java
@@ -3,7 +3,5 @@ package bstrom.akim.demoSpringBoot.exo;
 import org.springframework.beans.factory.InitializingBean;
 
 public interface InterfaceConsoleExo extends InitializingBean {
-
     void start();
-
 }
